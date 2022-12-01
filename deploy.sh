021a8596f7b48248cfc2dcd304c98c090889a834
James C Lloyd <jamesclloyd@Jamess-MacBook-Pro.local>
2022-12-01 14:26:57 -0500

    fixing last issue

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 python3 -m venv env
-
+python3 -m pip install --upgrade pip
 echo $PWD
 source env/bin/activate
-pip3 install -r requirements.txt
+
 python3 manage.py runserver 
 
 echo "server running"
