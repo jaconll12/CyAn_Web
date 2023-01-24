95415a6713394c55be911d378336d31afe8a3b43
James C Lloyd <jamesclloyd@Jamess-MacBook-Pro.local>
2023-01-24 08:46:33 -0500

    fixing issues

--- deploy.sh
+++ deploy.sh
@@ -3,7 +3,7 @@ python3 -m venv env
 source env/bin/activate
 pip install --upgrade pip
 pip install django
-pip install -r requirements.txt
+pip install -r reqs.txt
 echo $PWD
 source env/bin/activate
 
