4618c2ca78641223df155b461af156fa0f62e617
James C Lloyd <jamesclloyd@Jamess-MacBook-Pro.local>
2023-01-25 09:09:01 -0500

    fixing issues

--- deploy.sh
+++ deploy.sh
@@ -5,6 +5,7 @@ pip install --upgrade pip
 pip install django
 pip install django-sslserver
 pip install djangorestframework
+pip install mysqlclient
 #pip3 install django-sslserver
 #pip install -r reqs.txt
 echo $PWD
