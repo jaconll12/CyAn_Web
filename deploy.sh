91f1864076fd83bbeaf29510039275bb96fe48de
James C Lloyd <jamesclloyd@Jamess-MacBook-Pro.local>
2023-01-25 09:03:23 -0500

    fixing issues

--- deploy.sh
+++ deploy.sh
@@ -3,6 +3,8 @@ python3 -m venv env
 source env/bin/activate
 pip install --upgrade pip
 pip install django
+pip install django-sslserver
+pip3 install django-sslserver
 #pip install -r reqs.txt
 echo $PWD
 
