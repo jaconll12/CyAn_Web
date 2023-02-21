819176bf039b6f7838a1ae0b6954774e49ac26ff
James C Lloyd <jamesclloyd@Jamess-MacBook-Pro.local>
2023-02-21 15:46:57 -0500

    fixing jenkinsfile

--- build.sh
+++ build.sh
@@ -3,15 +3,22 @@ pip3 install virtualenv
 echo "1"
 virtualenv newenv
 source newenv/bin/activate
-
+pip3 install --upgrade pip
 pip3 install django
 pip3 install django-sslserver
 pip3 install djangorestframework
+pip3 install mysqlclient==2.1.0
+pip3 install requests==2.27.1
+pip3 install six==1.16.0
+pip3 installt hreadpoolctl==3.1.0
+pip3 installdj ango_heroku
+pip3 install  gunicorn
+pip3 install urllib3==1.26.9
 echo "2"
 django-admin --version
 echo "3"
-pip3 install --upgrade pip
-pip3 install -r requirements.txt
+
+
 
 echo "3"
 python3 manage.py migrate
