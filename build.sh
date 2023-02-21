86cc40179a2201c621286f176f5e13003856a478
James C Lloyd <jamesclloyd@Jamess-MacBook-Pro.local>
2023-02-21 15:41:31 -0500

    fixing jenkinsfile

--- build.sh
+++ build.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
-
+pip3 install virtualenv
 echo "1"
-
+virtualenv newenv
+source newenv/bin/activate
 echo "2"
+pip3 install django
+django-admin --version
 pip3 install --upgrade pip
 pip3 install -r requirements.txt
 
 echo "3"
 python3 manage.py migrate
 python3 manage.py test
-
+deactivate
 echo "build finished"
