c16490ce4e0cf5b3e8ee7d5fe8a7f9dd80a5dcda
James C Lloyd <jamesclloyd@Jamess-MacBook-Pro.local>
2023-02-21 15:44:51 -0500

    fixing jenkinsfile

--- build.sh
+++ build.sh
@@ -3,9 +3,13 @@ pip3 install virtualenv
 echo "1"
 virtualenv newenv
 source newenv/bin/activate
-echo "2"
+
 pip3 install django
+pip3 install django-sslserver
+pip3 install djangorestframework
+echo "2"
 django-admin --version
+echo "3"
 pip3 install --upgrade pip
 pip3 install -r requirements.txt
 
