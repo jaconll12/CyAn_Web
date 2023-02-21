4496c06a7b2a776cf6f784d3cb6eae400bb9a940
James C Lloyd <jamesclloyd@Jamess-MacBook-Pro.local>
2023-02-21 15:08:03 -0500

    fixing jenkinsfile

--- build.sh
+++ build.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 
 pip3 install virtualenv
+echo "1"
 virtualenv env
 source env/bin/activate
-pip install --upgrade pip
+echo "2"
+pip3 install --upgrade pip
 pip3 install -r requirements.txt
 
-python manage.py migrate
-python manage.py test
+echo "3"
+python3.10 manage.py migrate
+python3.10 manage.py test
 
 echo "build finished"
