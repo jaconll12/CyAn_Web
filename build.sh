75d02b4ba15ea97fd3b72a971205b482ab2b6896
James C Lloyd <jamesclloyd@Jamess-MacBook-Pro.local>
2023-02-22 14:07:51 -0500

    fixing

--- build.sh
+++ build.sh
@@ -7,10 +7,10 @@ pip3 install --upgrade pip
 pip3 install django
 pip3 install django-sslserver
 pip3 install djangorestframework
+pip3 install mysql==0.0.3
+pip3 install mysqlclient==2.1.0
 pip3 install mysql-connector
 pip3 install mysql-connector-python
-pip3 install mysqlclient
-pip install mysqlclient
 pip3 install requests==2.27.1
 pip3 install six==1.16.0
 pip3 install threadpoolctl==3.1.0
