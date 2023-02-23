04a758b95e78b0c078736aa3c420708ba23a4071
James C Lloyd <jamesclloyd@Jamess-MacBook-Pro.local>
2023-02-23 14:29:26 -0500

    fix

--- build.sh
+++ build.sh
@@ -1,18 +1,15 @@
 #!/bin/bash
 #!/opt/homebrew/bin/python3
 echo "1"
-PYTHONPATH="/opt/homebrew/bin/python3"
-export PYTHONPATH
-python3.10 -V
-python3.10 -m pip install --user --upgrade pip
-python3.10 -m pip install --user virtualenv
-python3.10 -m venv venv
+python3 -V
+python3 -m pip install --user --upgrade pip
+python3 -m pip install --user virtualenv
+python3 -m venv venv
 source venv/bin/activate
 pip3 install --upgrade pip
 export PATH=/usr/local/mysql-8.0.32-macos13-arm64/bin:$PATH 
 echo "2"
 pip3 install -r requirements.txt 
-
 
 
 
