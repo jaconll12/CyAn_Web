7804363c3c4518503ea5fe3db0e5af6dcedb34be
James C Lloyd <jamesclloyd@Jamess-MacBook-Pro.local>
2023-02-24 20:25:04 -0500

    realigning JenkinsFile

--- test.sh
+++ test.sh
@@ -2,20 +2,9 @@
 #!/opt/homebrew/bin/python3
 python3 -V
 which python3
-python3 -m pip install --upgrade pip
-python3 -m pip install virtualenv
-python3 -m virtualenv venv
 source venv/bin/activate
-python3 -m venv --upgrade ENV_DIR
-pip3 install --upgrade pip
-PATH=/usr/local/mysql-8.0.32-macos13-arm64/bin:$PATH
-export PATH
-PYTHONPATH=/opt/homebrew/bin/python3
-export PYTHONPATH
-python3 -V
-pip3 install -r requirements.txt 
 django-admin --version
 python3 manage.py migrate
 python3 manage.py test
 deactivate
-echo "build finished"
+echo "test finished"
