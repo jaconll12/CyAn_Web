9bcead6e2ef14c54a452135407ef2fb4acfe69f5
James C Lloyd <jamesclloyd@Jamess-MacBook-Pro.local>
2023-02-28 15:04:44 -0500

    fixing issues

--- test.sh
+++ test.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-#!/opt/homebrew/bin/python3
-python3 -V
+#!/opt/homebrew/bin/
+/opt/homebrew/bin/python3 -V
 which python3
 source venv/bin/activate
 django-admin --version
-python3 manage.py migrate
-python3 manage.py test
+/opt/homebrew/bin/python3 manage.py migrate
+/opt/homebrew/bin/python3 manage.py test
 deactivate
 echo "test finished"
