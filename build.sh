c11dce677009f2c0aafb4a71abbb2090a849f7c1
James C Lloyd <jamesclloyd@Jamess-MacBook-Pro.local>
2023-02-28 15:00:18 -0500

    fixing python issue

--- build.sh
+++ build.sh
@@ -3,16 +3,17 @@
 /opt/homebrew/bin/python3 -V
 which python3
 echo $PYTHONPATH
-python3 -m pip install --upgrade pip
-python3 -m pip install virtualenv
-python3 -m virtualenv venv
+/opt/homebrew/bin/python3 -m pip install --upgrade pip
+/opt/homebrew/bin/python3 -m pip install virtualenv
+/opt/homebrew/bin/python3 -m virtualenv venv
 source venv/bin/activate
-python3 -m venv --upgrade ENV_DIR
-pip3 install --upgrade pip
+/opt/homebrew/bin/python3 -m venv --upgrade ENV_DIR
 PATH=/usr/local/mysql-8.0.32-macos13-arm64/bin:$PATH
 export PATH
 PYTHONPATH=/opt/homebrew/bin/python3
 export PYTHONPATH
+pip3 install --upgrade pip
+
 python3 -V
 pip3 install -r requirements.txt 
 django-admin --version
