b96e261350e9ae88d3e83795ea8d38d43f2ce128
James C Lloyd <jamesclloyd@Jamess-MacBook-Pro.local>
2023-03-01 13:21:43 -0500

    another one

--- build.sh
+++ build.sh
@@ -5,21 +5,21 @@ which python3
 echo $PYTHONPATH
 /opt/homebrew/bin/python3 -m pip install --upgrade pip
 /opt/homebrew/bin/python3 -m pip install virtualenv
-/opt/homebrew/bin/python3 -m pip install venv
 
 
-/opt/homebrew/bin/python3 -m venv cyan-env
+
+/opt/homebrew/bin/python3 -m virtualenv cyan-env
 source yan-env/bin/activate
 
 PATH=/usr/local/mysql-8.0.32-macos13-arm64/bin:$PATH
-PATH=/opt/homebrew/bin
+PATH=/opt/homebrew/bin:$PATH
 export PATH
 PYTHONPATH=/opt/homebrew/bin/python3
 export PYTHONPATH
 pip3 install --upgrade pip
 
 python3 -V
-pip3 install -r requirements.txt 
+/opt/homebrew/bin/python3 -m pip install -r requirements.txt 
 /opt/homebrew/bin/python3 -m pip install django
 django-admin --version
 /opt/homebrew/bin/python3 manage.py migrate
