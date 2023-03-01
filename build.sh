4bcfcedef596753c8af549d905a819146079604e
James C Lloyd <jamesclloyd@Jamess-MacBook-Pro.local>
2023-03-01 10:20:19 -0500

    another one

--- build.sh
+++ build.sh
@@ -9,6 +9,7 @@ echo $PYTHONPATH
 source venv/bin/activate
 /opt/homebrew/bin/python3 -m venv --upgrade ENV_DIR
 PATH=/usr/local/mysql-8.0.32-macos13-arm64/bin:$PATH
+PATH=/opt/homebrew/bin
 export PATH
 PYTHONPATH=/opt/homebrew/bin/python3
 export PYTHONPATH
