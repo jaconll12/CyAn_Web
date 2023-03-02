7321f33cc9be8626a75aa1750e71d2b7db485b9a
James C Lloyd <jamesclloyd@Jamess-MacBook-Pro.local>
2023-03-02 15:50:29 -0500

    remote test

--- deploy.sh
+++ deploy.sh
@@ -2,8 +2,18 @@
 #!/opt/homebrew/bin/python3
 python3 -V
 which python3
-source venv/bin/activate
+
+source cyan-env/bin/activate
+
+PATH=/usr/local/mysql-8.0.32-macos13-arm64/bin:$PATH
+PATH=/opt/homebrew/bin:$PATH
+export PATH
+PYTHONPATH=/opt/homebrew/bin/python3
+export PYTHONPATH
+
+python3 -V
 django-admin --version
+
 python3 manage.py runserver
 deactivate
 echo "test finished"
